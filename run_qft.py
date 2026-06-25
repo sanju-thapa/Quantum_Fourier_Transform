aeafa096009508fa7bf467e805d8565d6aad0e46
Sanju Thapa <291080708+sanju-thapa@users.noreply.github.com.>
2026-06-25 13:50:55 +0530

    Add QFT implementation with state prep and statevector verification

--- run_qft.py
+++ run_qft.py
@@ -3,8 +3,21 @@ from qiskit import QuantumCircuit
 from qiskit.quantum_info import Statevector
 
 def run_qft(n, input_state):
+    #1. Initialize the quantum circuit with 'n' qubits
     qc = QuantumCircuit(n)
-    for i in range(n):
-        if (input_state >> i) & 1:
-            qc.x(i)
 
+    #2. State preparation: Encode the integer into binary qubits 
+    # Loop through each qubit index
+    for i in range(n):
+        # Extract the i-th bit of the integer using right-shift and bitwise AND
+        if (input_state >> i) & 1:
+            qc.x(i) # Apply the X gate if the bit is 1
+
+     # Append the QFT operation to the circuit
+    qc.append(qft(n), range(n))
+    sv = Statevector(qc)
+    print(sv)
+    return qc 
+
+circuit = run_qft(3,5)
+print(circuit.decompose().draw())
